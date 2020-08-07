3cffc10b916f6cee4b92ed839ab9bad2d5e4867d
maoux <h.eynard@orange.fr>
2020-08-07 22:31:26 +0200

    some config abstraction for ld in Makefile configuration

--- config/config.mk
+++ config/config.mk
@@ -1,8 +1,7 @@
 PROJECT_NAME	:= kfs1
 NAME			?= kfs.bin
-
+NAME_ISO		?= kfs.iso
 ARCH			?= x86
-
 MAKE_EXT		?= .mk
 
 #directories
@@ -19,12 +18,13 @@ SCRIPTS_DIR		:= $(PROJECT_DIR)/scripts
 #compiler / asm
 CC			:= /usr/bin/gcc
 AS			:= /usr/bin/nasm
-
+LD			:= /usr/bin/ld
+LDFILE		:= $(ARCH_DIR)/$(ARCH)/linker.ld
 
 #flags
-LDFLAGS		:= -T linker.ld -m elf_i386
+LDFLAGS		:= -T $(LDFILE) -m elf_i386
 ASFLAGS		:= -f elf32
 CFLAGS		:= -Wall -Wextra -Werror -m32 -O2
-CRCFLAGS	:= -fno-builtin -nostdlib -nodefaultlibs -fno-stack-protector
+CRCFLAGS	:= -ffreestanding -fno-builtin -nostdlib -nodefaultlibs -fno-stack-protector
 IFLAGS		:= -I $(INC_DIR)
 DEBUGFLAGS	:= -g
